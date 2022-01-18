deb1aff7498600929386a1401c569ac9d9cb4512
mvvrachev <martin.v.vrachev.2017@elsys-bg.org>
2022-01-18 14:07:29 +0200

    Fixing merge conflict errors

--- DjangoForum/DjangoForum/settings.py
+++ DjangoForum/DjangoForum/settings.py
@@ -20,6 +20,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'forum.apps.ForumConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -103,25 +104,20 @@ USE_I18N = True
 
 USE_TZ = True
 
-<<<<<<< Updated upstream
-=======
-STATIC_URL = '/static/'
->>>>>>> Stashed changes
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
 
-<<<<<<< Updated upstream
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-=======
+
 LOGIN_URL = 'login'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
->>>>>>> Stashed changes
+
--- DjangoForum/forum/forms.py
+++ DjangoForum/forum/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
 
 class UserRegistrationForm(forms.Form):
     username = forms.CharField(max_length=20)
@@ -9,9 +10,9 @@ class UserRegistrationForm(forms.Form):
 
     class Meta:
         model = User
-<<<<<<< Updated upstream
+
         fields = ['username', 'email', 'password1', 'password2']
-=======
+
         fields = ['username', 'email', 'password1', 'password2']
 
 
@@ -27,4 +28,4 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['image']
->>>>>>> Stashed changes
+
