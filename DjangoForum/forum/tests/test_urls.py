d7a07c6e7cd5d20b633c72fad8d1660937eeed8f
mvvrachev <72374789+mvvrachev@users.noreply.github.com>
2022-04-20 18:16:20 +0300

    Add tests for comments
    * Add comments
    
    * Add tests for comments

--- DjangoForum/forum/tests/test_urls.py
+++ DjangoForum/forum/tests/test_urls.py
@@ -1,6 +1,6 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from forum.views import PostCreateView, PostDeleteView, PostDetailView, PostUpdateView, TopicListView, TopicDetailView, TopicCreateView, TopicUpdateView, TopicDeleteView
+from forum.views import PostCreateView, PostDeleteView, PostDetailView, PostUpdateView, TopicListView, TopicDetailView, TopicCreateView, TopicUpdateView, TopicDeleteView, CommentCreateView, CommentDeleteView, CommentDetailView, CommentUpdateView
 
 class TestUrls(SimpleTestCase):
 
@@ -39,3 +39,19 @@ class TestUrls(SimpleTestCase):
     def test_topic_delete_url_is_resolved(self):
         url = reverse('topic-delete', args=[1])
         self.assertEquals(resolve(url).func.view_class, TopicDeleteView)
+
+    def test_comment_detail_url_is_resolved(self):
+        url = reverse('comment-detail', args=[1])
+        self.assertEquals(resolve(url).func.view_class, CommentDetailView)
+
+    def test_comment_create_url_is_resolved(self):
+        url = reverse('comment-create', args=[1])
+        self.assertEquals(resolve(url).func.view_class, CommentCreateView)
+
+    def test_comment_udpate_url_is_resolved(self):
+        url = reverse('comment-update', args=[1])
+        self.assertEquals(resolve(url).func.view_class, CommentUpdateView)
+
+    def test_comment_delete_url_is_resolved(self):
+        url = reverse('comment-delete', args=[1])
+        self.assertEquals(resolve(url).func.view_class, CommentDeleteView)
